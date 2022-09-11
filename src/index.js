cd3b1bfab2ab5745b936fc3088fdb22895404cd4
hagi-3 <hagi39077@gmail.com>
2022-09-11 14:42:21 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+{
+  const user = {
+    name: "nobu",
+    age: 26
+  };
+  document.querySelector(".js-name").textContent = user.name;
+  document.querySelector(".js-age").textContent = user.age;
+}
+
+const onClickPlusAge = () => {
+  const age = document.querySelector(".js-age").textContent;
+  document.querySelector(".js-age").textContent = Number(age) + 1;
+};
+
+const onClickMinusAge = () => {
+  const age = document.querySelector(".js-age").textContent;
+  document.querySelector(".js-age").textContent = Number(age) - 1;
+};
+
+document
+  .querySelector(".js-age-plus-button")
+  .addEventListener("click", () => onClickPlusAge());
+
+document
+  .querySelector(".js-age-plus-button")
+  .addEventListener("click", () => onClickMinusAge());
